5183690cdd5762fa9a3efe78e64e5d0489178cf0
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-20 18:36:39 +0530

    Done the debouncing part.

---
+++ rough.js
@@ -0,0 +1,21 @@
+// const arr = ['E>R', 'R>U', 'P>E'];
+// ['I>T', 'S>W', 'T>Z', 'W>I', 'Z>E', 'N>D', 'R>L', 'E>R', 'A>N', 'L>A']
+
+// const roughFun = () => {
+//     var str = "";           
+//     var before = "";
+//     var after = "";
+//     var newStr = "";
+//     arr.map((item) => {
+//         newStr = `${item[0]}${item[2]}`;
+//         if(str) {
+//             if(str[0] == newStr[1]){
+//                 str = `${newStr[0]}${str}`
+//             }else if()
+//         }
+//         str = str + `${item[0]}${item[2]}`
+//         console.log(str);
+//     })
+// }
+
+// console.log(roughFun());
--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+const [text, setText] = useState(null);
+const [data, setData] = useState();
+
+useEffect(() => {
+  const timeoutId = setTimeout(() => {
+    axios.get(`https://609cd6ba04bffa001792d638.mockapi.io/books?search=${text}`)
+      .then((res) => {
+        const arr = res.data;
+        setData(arr);
+        if(arr.length === 0){
+          setData([]);
+          console.log("No data found = "+text);
+        }
+      })
+  }, 1000);
+  return () => clearTimeout(timeoutId);
+}, [text])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" style={{marginTop:"20px"}} onChange={(e) => 
+        // handleChange(e.target.value)
+        setText(e.target.value)
+        }></input>
+      <div style={{marginTop:"20px", display:"flex", flexWrap: "wrap", justifyContent: "space-between", margin:"0 20px"}}>
+        {/* {console.log(data)} */}
+        {data && data.length > 0 && data.map((item, key) => (
+          <div key={key}  style={{width:"350px", margin:"0 10px"}}>
+            <div style={{display:"flex", justifyContent: "space-between"}}>
+              <h3>{item.book_author}</h3>
+              <h5>{item.name}</h5>
+            </div>
+            <div>
+              <img src={item.book_image} width="100%"/>
+            </div>
+            <div>
+              <h2><i>Price: </i>{item.price}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
