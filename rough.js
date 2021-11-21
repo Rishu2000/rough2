3270d7e9b0b07a811c6670ba65808cac3727cb56
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-21 12:27:02 +0530

    Got the greatest character from the array.

--- rough.js
+++ rough.js
@@ -1,8 +1,23 @@
 // ['E>R', 'R>U', 'P>E'] => PERU; 
 // ['I>T', 'S>W', 'T>Z', 'W>I', 'Z>E', 'N>D', 'R>L', 'E>R', 'A>N', 'L>A'] => SWITZERLAND
 
-const roughFun = () => {
+const roughFun = (arr) => {
+    // console.log(arr);
+    var greaterArr = [];
+    var smallerArr = [];
+    arr.map((item) => {
+        greaterArr.push(item[0]);
+        smallerArr.push(item[2]);
+    })
+    // console.log(greaterArr);
+    // console.log(smallerArr);
+    greaterArr.map((item) => {
+        if(!(smallerArr.find((item2) => item === item2))){
+            console.log(item);
+        }
+    })
     return "Working";
 }
 
-console.log(roughFun());
+console.log(roughFun(['I>T', 'S>W', 'T>Z', 'W>I', 'Z>E', 'N>D', 'R>L', 'E>R', 'A>N', 'L>A']));
+console.log(roughFun(['E>R', 'R>U', 'P>E']));
